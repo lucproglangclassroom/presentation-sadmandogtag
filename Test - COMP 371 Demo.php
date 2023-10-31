91bed2907c4d8e96b74a2535070c491908d52fd9
sadmandogtag <67683283+sadmandogtag@users.noreply.github.com>
2023-10-31 11:50:31 -0500

    Add files via upload

---
+++ Test - COMP 371 Demo.php
@@ -0,0 +1,22 @@
+<?php
+// PHP code to generate HTML with a PHP logo image
+$html = '
+<!DOCTYPE html>
+<html>
+<head>
+    <title>PHP-Generated HTML</title>
+</head>
+<body>
+    <h1>Hello from PHP-Generated HTML!</h1>
+    <img src="https://www.php.net/images/logos/php-med-trans.png" alt="PHP Logo">
+</body>
+</html>
+';
+
+// Save the generated HTML to an HTML file
+$file = 'generated.html';
+file_put_contents($file, $html);
+
+// Print a message indicating that the HTML file has been generated
+echo "HTML file '$file' has been generated.\n";
+?>
